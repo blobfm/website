479ae164a4c93197f065bb9ff93ee72d5f60541a
Philippe Schommers <philippe@schommers.be>
2024-03-10 21:20:12 +0100

    chore: cleanup unused variable

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,9 +2,6 @@ export const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve
 
 export const hexToUint8Array = (hex: string) => {
     const cleaned = hex.startsWith("0x") ? hex.substring(2) : hex;
-    const decoded = cleaned
-        .match(/../g)
-        ?.flatMap((h: string, i: number) => parseInt(h, 16));
-
+    const decoded = cleaned.match(/../g)?.flatMap((h: string) => parseInt(h, 16));
     return Uint8Array.from(decoded ?? []);
 };
