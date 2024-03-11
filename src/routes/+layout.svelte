fd1b3f0a508b4473dcb16c6e1da297b7e710b7de
Philippe Schommers <philippe@schommers.be>
2024-03-11 01:47:58 +0100

    feat: add creators

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,23 @@
 <script>
 	import '../app.css';
+
+	// Components
 	import NavItem from '../components/nav-item.svelte';
+
+	// Icons
+	import EthereumIcon from '~icons/mdi/ethereum';
 </script>
 
 <header class="text-center">
 	<img src="/logo.png" alt="Blob.fm" class="m-auto mt-4 mb-2" />
-	<p>Home of Blobs and Beats.</p>
+
+	<p>Home of Blobs and Beats</p>
+
+	<div class="flex items-center justify-center my-4 gap-2">
+		With <EthereumIcon class="inline text-red-500" /> by
+		<a class="underline" href="https://twitter.com/filoozom">Philippe</a> and
+		<a class="underline" href="https://twitter.com/davekpk">Dave</a>
+	</div>
 </header>
 
 <nav>
